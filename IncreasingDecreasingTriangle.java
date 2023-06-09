97a703564cd98dbc8a714e3e2a153072c6427dac
RohitWorkSpace <133697257+RohitWorkSpace@users.noreply.github.com>
2023-06-10 00:19:49 +0530

    Add files via upload

---
+++ IncreasingDecreasingTriangle.java
@@ -0,0 +1,36 @@
+package patternprogramming;
+
+public class IncreasingDecreasingTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 5;
+		for (int i = 0; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = 0; i <= n; i++) {
+			for (int j = i; j <= n; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+//result:-
+//*
+//**
+//***
+//****
+//*****
+//******
+//*****
+//****
+//***
+//**
+//*
